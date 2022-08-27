5d3c0e423b9a4c364c394bc0c03076f3c40365c1
Normanfamdamly <104736546+Normanfamdamly@users.noreply.github.com>
2022-08-27 12:22:30 -0400

    9.5.2

--- Weather_app/app.py
+++ Weather_app/app.py
@@ -7,3 +7,31 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import FLASK, jasonify
 
+engine=create_engine("sqlite://hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Measurement=Base.classes.measurement
+Statation=Base.classes.station
+session=Session(engine)
+
+app= Flask(__name__)
+
+import app
+print("example__name__%s" __name__)
+
+if__name__=="__main__":
+    print("example is being run directly")
+else:
+    print("example is being imported")
+
+@app.rount("/")
+def welcome()
+    return(
+        '''
+        Welcome to the Climate Analysis API!
+        Available Routes:
+        /api/v1.0/precipitation
+        /api/v1.0/stations
+        /api/v1.0/tobs
+        /api/v1.0/temp/start/end
+        ''')
