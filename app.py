0ff82f43dd848ea7e6eb147167bc8d58630ef32b
Normanfamdamly <104736546+Normanfamdamly@users.noreply.github.com>
2022-08-27 13:25:17 -0400

    updated

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import datetime as dt
 import numpy as np
 import pandas as pd
 import sqlalchemy
-from sqlalchemy.ext.automap import automap_map
+from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-from flask import FLASK, jasonify
+from flask import FLASK, jsonify
 
 engine=create_engine("sqlite://hawaii.sqlite")
 Base = automap_base()
