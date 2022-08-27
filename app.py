ffb828f544ab8ea2c1dd8a21fc6a10a2e9ee43ac
Normanfamdamly <104736546+Normanfamdamly@users.noreply.github.com>
2022-08-27 13:46:48 -0400

    fixed?

--- app.py
+++ app.py
@@ -5,16 +5,17 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+
 from flask import Flask, jsonify
 
-engine=create_engine("sqlite://hawaii.sqlite")
+engine=create_engine("sqlite:///hawaii.sqlite")
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 Measurement=Base.classes.measurement
-Statation=Base.classes.station
+Station=Base.classes.station
 session=Session(engine)
 
-app= FLASK(__name__)
+app= Flask(__name__)
 
 @app.rount("/")
 def welcome():
