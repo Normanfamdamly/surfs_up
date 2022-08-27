e95e1a1a354991e8e93023714043259ced57d993
Normanfamdamly <104736546+Normanfamdamly@users.noreply.github.com>
2022-08-27 13:13:14 -0400

    Redo

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from cProfile import run
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return 'Hello World, my first website'
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_map
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import FLASK, jasonify
+
+engine=create_engine("sqlite://hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Measurement=Base.classes.measurement
+Statation=Base.classes.station
+session=Session(engine)
+
+app= Flask(__name__)
+
+@app.rount("/")
+def welcome():
+    return(
+        '''
+        Welcome to the Climate Analysis API!
+        Available Routes:
+        /api/v1.0/precipitation
+        /api/v1.0/stations
+        /api/v1.0/tobs
+        /api/v1.0/temp/start/end
+        ''')
 
