fa2d190f8791b53fefff6cc916244672ca9cbd38
Normanfamdamly <104736546+Normanfamdamly@users.noreply.github.com>
2022-08-27 13:05:45 -0400

    fixed spelling

--- Weather_app/app.py
+++ Weather_app/app.py
@@ -2,7 +2,7 @@ import datetime as dt
 import numpy as np
 import pandas as pd
 import sqlalchemy
-from sqlalchemy.ext.automap import autmap_map
+from sqlalchemy.ext.automap import automap_map
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import FLASK, jasonify
